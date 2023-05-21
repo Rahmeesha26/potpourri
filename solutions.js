4c1588b1cb48f4a05702769ec5293fa7b8ac5649
rahmeesha26 <rahmeeshar91@gmail.com>
2023-05-20 20:16:57 -0400

    I didn't get ti finish

---
+++ solutions.js
@@ -0,0 +1,28 @@
+// SET ALARM
+//////////////////////////////////////////////////////////////////////////////////////////////
+// let employed = 7;
+// let vacationing = 2;
+// function setAlarm(employed, vacationing){
+//           return employed === vacationing || vacationing < employed
+// }
+// let shouldSetAlarm = setAlarm(employed, vacationing);
+// console.log("Set alarm", shouldSetAlarm)
+
+//COUNT ODD NUMBERS
+//////////////////////////////////////////////////////////////////////////////////////////////
+let odd = 1, c = 50;
+let count = 0;
+
+ function oddNumberCount(){
+          for(odd = 1; odd <= c; odd++)
+                  if(odd % 2 != 0)  
+          count++;
+}
+oddNumberCount();
+
+// DISEMVOWING TROLLS
+//////////////////////////////////////////////////////////////////////////////////////////////
+
+function trollsBeGone(){
+
+}
